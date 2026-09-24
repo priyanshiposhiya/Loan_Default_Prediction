85bd55267cd5c009a17e9d76319841c4956d3fbf
Priyanshi Poshiya <priyanshiposhiya53@gmail.com>
2026-09-24 23:18:35 +0530

    Connect frontend to deployed API

--- Frontend/Loan_Default_Prediction/src/pages/Prediction.jsx
+++ Frontend/Loan_Default_Prediction/src/pages/Prediction.jsx
@@ -123,7 +123,7 @@ const Prediction = () => {
     setErrorMessage("");
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/api/predict", {
+      const response = await fetch("https://loan-default-prediction-65ud.onrender.com/api/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
